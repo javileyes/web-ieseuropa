75ed1cdcaa135e72cb67eea86dc529fc0206959a
somaxa8 <silviofrancoxa8@gmail.com>
2021-01-26 20:17:53 -0500

    added vue-fullscreen module

--- src/components/panel/PostBlogPanel.vue
+++ src/components/panel/PostBlogPanel.vue
@@ -1,48 +1,52 @@
 <template>
-    <v-card>
-        <v-toolbar color="secondary" dark>
-            <v-toolbar-title>Crear Noticia</v-toolbar-title>
-        </v-toolbar>
-        <v-progress-linear :indeterminate="true" :active="loading" color="warning"/>
-        <v-form ref="form">
-            <v-container>
-                <v-row>
-                    <v-col>
-                        <v-text-field v-model="title" label="Titulo" :rules="titleRules" filled clearable />
+    <Fullscreen ref="fullscreen" @change="fullscreenChange">
+        <v-card height="100%">
+            <v-toolbar color="secondary" dark>
+                <v-toolbar-title>Crear Noticia</v-toolbar-title>
+                <v-spacer/>
+                <v-btn icon @click="$refs['fullscreen'].toggle()">
+                    <v-icon>{{ fullscreen ? "mdi-arrow-collapse" : "mdi-arrow-expand" }}</v-icon>
+                </v-btn>
+            </v-toolbar>
+            <v-progress-linear :indeterminate="true" :active="loading" color="warning"/>
+            <v-form ref="form">
+                <v-container>
+                    <v-row>
+                        <v-col>
+                            <v-text-field v-model="title" label="Titulo" :rules="titleRules" filled clearable />
 
-                        <v-textarea filled name="input-7-4" label="Cuerpo" v-model="body" :rules="bodyRules"/>
+                            <v-textarea filled name="input-7-4" label="Cuerpo" v-model="body" :rules="bodyRules"/>
 
-                        <v-select
-                            :items="labels" filled
-                            label="Etiqueta" v-model="label"
-                        >
-                            <template v-slot:item="{item}">
-                                {{item.title}}
-                            </template>
-                            <template v-slot:selection="{item}">
-                                {{item.title}}
-                            </template>
-                        </v-select>
+                            <v-select :items="labels" filled label="Etiqueta" v-model="label">
+                                <template v-slot:item="{item}">
+                                    {{item.title}}
+                                </template>
+                                <template v-slot:selection="{item}">
+                                    {{item.title}}
+                                </template>
+                            </v-select>
 
-                        <v-btn block :loading="loading" :disabled="loading" color="success" @click="postBlog">
-                            Crear
-                            <template v-slot:loader>
-                                <span>Loading...</span>
-                            </template>
-                        </v-btn>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-form>
-    </v-card>
+                            <v-btn block :loading="loading" :disabled="loading" color="success" @click="postBlog">
+                                Crear
+                                <template v-slot:loader>
+                                    <span>Loading...</span>
+                                </template>
+                            </v-btn>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-form>
+        </v-card>
+    </Fullscreen>
 </template>
 
 <script lang="ts">
 import {Component, Prop, Ref, Vue} from "vue-property-decorator";
 import BlogLabel from "@/models/BlogLabel";
 import BlogService from "@/services/BlogService";
+import Fullscreen from "vue-fullscreen/src/component"
 
-@Component
+@Component({components:{Fullscreen}})
 export default class PostBlogPanel extends Vue {
     @Ref() readonly form!: HTMLFormElement
     @Prop() readonly refresh!: any
@@ -53,6 +57,12 @@ export default class PostBlogPanel extends Vue {
     body: string = ""
     titleRules = [(v: string) => v && v.length > 0 ? true : "Titulo requerido"]
     bodyRules = [(v: string) => v && v.length > 0 ? true : "Cuerpo requerido"]
+    fullscreen: boolean = true
+
+
+    fullscreenChange(fullscreen: boolean) {
+        this.fullscreen = fullscreen
+    }
 
     postBlog() {
         if (this.form.validate()) {
