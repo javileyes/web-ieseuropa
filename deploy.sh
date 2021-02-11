fa85f464b4cb59956a5e07ed0752d0fe474cb09a
javiergimenezmoya <javiergimenezmoya@gmail.com>
2021-02-11 22:49:12 +0100

    deploy OVH

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ npm run build
 #ESTO LO COMENTO PORQUE SE DEBE HACER SOLO UNA VEZ
 #eval `ssh-agent` #ESTO ES PARA NO TENER QUE PONER CONTINUAMENTE EL PASSWORD DE ENCRIPTACIÓN DE LA CLAVE PRIVADA
 #ssh-add          #SI LA CLAVE NO ESTÁ EN SU SITIO POR DEFECTO PONER: ssh-add /home/test/.ssh/id_rsa
-ssh root@javiergimenez.es rm -rf /root/ieseuropa/nginx-docker/html/*
-scp -r dist/* root@javiergimenez.es:/root/ieseuropa/nginx-docker/html
+ssh root@ieseuropa.es rm -rf /root/ieseuropa/nginx-docker/html/*
+scp -r dist/* root@ieseuropa.es:/root/ieseuropa/nginx-docker/html
+
