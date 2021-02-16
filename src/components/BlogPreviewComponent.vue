c93cbcf21f8fb8ee704baaa571bfa7676f66e4d4
javiergimenezmoya <javiergimenezmoya@gmail.com>
2021-02-16 17:15:36 +0100

    No cortar palabras de los títulos

--- src/components/BlogPreviewComponent.vue
+++ src/components/BlogPreviewComponent.vue
@@ -3,7 +3,7 @@
         <v-img v-if="blog.images.length" class="white--text align-end" height="200px" :src="blog.images[0].url"/>
         <v-img v-else class="white--text align-end" height="200px" :src="blog.label.image.url"/>
         <v-card-subtitle style="text-align: end; color: purple; margin: -5px">{{ blog.label.title }}</v-card-subtitle>
-        <v-card-title style="color: blue; margin-top: -20px; margin-bottom: -15px">{{ blog.title.slice(0, 62) }}</v-card-title>
+        <v-card-title style="word-break:keep-all; color: blue; margin-top: -20px; margin-bottom: -15px">{{ blog.title.slice(0, 62) }}</v-card-title>
         <v-divider class="mx-4"/>
         <v-card-subtitle class="pb-0">
             {{ date(blog.createdAt).replace("$", "de") }}
